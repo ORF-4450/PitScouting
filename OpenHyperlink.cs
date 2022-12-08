b9995669a6202e298d890ef240f7e427abb65942
Sammy-Da-Great <sammyhusma@gmail.com>
2022-12-07 16:57:24 -0800

    Added comments to OpenHyperlink

--- OpenHyperlink.cs
+++ OpenHyperlink.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//To Use: Attach script to an object.
+//Fill out hyperlink field with your link.
+//Add a button component to your object.
+//As an action, call the Openchannel function in the script, in the object.
+
 public class OpenHyperlink : MonoBehaviour
 {
 
+    //Adds field for a hyperlink
     public string Hyperlink;
 
     public void Openchannel()
     {
+        //Opens URL Hyperlink
         Application.OpenURL(Hyperlink);
     }
 }
