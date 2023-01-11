0b50bc0250079330a05fccb6fa55d63728911bf3
Sammy-Da-Great <sammyhusma@gmail.com>
2023-01-10 17:23:43 -0800

    Commented FlipImageScript

--- FlipImageScript.cs
+++ FlipImageScript.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script flips the position selection area using one of two animations.
+//One spins clockwise (CW), the other spins it counterclockwise (CCW)
+
 public class FlipImageScript : MonoBehaviour
 {
     public GameObject Button;
@@ -28,17 +31,19 @@ public class FlipImageScript : MonoBehaviour
 
     public void Flip()
     {
-        if (!Button.GetComponent<Animation>().IsPlaying("ButtonPressed"))
+        if (!Button.GetComponent<Animation>().IsPlaying("ButtonPressed")) //If the button NOT is playing it's "pressed" animation
         {
-            Button.GetComponent<Animation>().Play("ButtonPressed");
-            if (flipped == true)
+            Button.GetComponent<Animation>().Play("ButtonPressed"); //Play the button animation
+            if (flipped == true) //If that was true, Find out if it has been flipped or not
             {
-                GetComponent<Animation>().Play("SelectionButtonSpinCW");
+                GetComponent<Animation>().Play("SelectionButtonSpinCW"); //If it has been flipped, unflip it
             } else {
-                GetComponent<Animation>().Play("SelectionButtonSpinCCW");
+                GetComponent<Animation>().Play("SelectionButtonSpinCCW"); //If it hasn't been flipped, flip it
             }
-            Animation anim = GetComponent<Animation>();
-            flipped = !(flipped);
+
+            Animation anim = GetComponent<Animation>(); //Not sure if this does anything anymore.
+            
+            flipped = !flipped; //Change the boolean for flipping to tell the script whether or not the buttons have been flipped.
         }
         
     }
