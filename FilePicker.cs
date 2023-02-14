b48133f23ce04778199e777aec2f292c25bd459b
Sammy-Da-Great <sammyhusma@gmail.com>
2023-02-13 16:17:22 -0800

    Organized Code

--- FilePicker.cs
+++ FilePicker.cs
@@ -20,22 +20,27 @@ public class FilePicker : MonoBehaviour {
 
 	//This script runs the file selection menu.
 
+//#Show Menus. This allows the script to load data into *every* object when data is loaded.
 	public void loadMenu () { //Set hardcoded objects' SetActive bool to true, making them appear.
 		FileMenu.SetActive (true);
 		MainMenu.SetActive (true);
 		ScoutingMenu.SetActive (true);
 	}
 
+//#Set DS to <DataStorage> component
 	public void Start() {
 		DS = GetComponent<DataStorage>();
 	}
-
+//
 	public void LateUpdate() {
+	//#Stop if dropdown has unexpected value
 		if (dropdown == null)
 			return;
 		if (dropdown.value == 0 || dropdown.options[dropdown.value].text == ".DS_Store")
 			return;
+	//
 		FileInfo tmp_file = new FileInfo (Application.persistentDataPath + Path.DirectorySeparatorChar + dropdown.captionText.text);
+	//#If file is empty, set file to tmp_file
         if (file == null)
         {
             if (tmp_file.Exists)
@@ -44,21 +49,23 @@ public class FilePicker : MonoBehaviour {
                 dataViewer.text = getStringFromFile(file);
             }
         }
+	//If file does not exist and is not null, set dropdown to 0 and stop.
         if (file != null && !file.Exists)
         {
             dropdown.value = 0;
             dropdown.RefreshShownValue();
             return;
         }
+	//If file equals tmp_file, stop. Else, set file to tmp_file
 		if (tmp_file.Equals(file))
 			return;
 		file = tmp_file;
-//Read .TXT
+	//Read .TXT
 		if (file.Extension == ".txt") //If the file is a .txt, read as text
 		{
 			dataViewer.text = getStringFromFile(file);
 		}
-//Read PNG
+	//Read PNG
 		if (file.Extension == ".png") //if the file is a .png, read as a picture
 		{			
 			DisplayedImage.enabled = true; //Enable the RawImage 
@@ -77,16 +84,20 @@ public class FilePicker : MonoBehaviour {
 	}
 
 	public void Update() {
+	//#Stop if dropdown has unexpected value
 		if (dropdown == null)
 			return;
+	//
 		List<Dropdown.OptionData> files = new List<Dropdown.OptionData>();
 		DirectoryInfo directory = new DirectoryInfo (Application.persistentDataPath);
 		files.Add (new Dropdown.OptionData("Choose a file"));
+	//#Add File names to "files"
 		foreach (FileInfo file in directory.GetFiles()) {
 			if (file.Name.StartsWith (".") || file.Extension == ".json")
 				continue;
 			files.Add (new Dropdown.OptionData (file.Name));
 		}
+	//#Set dropdown values to "files"
 		if (dropdown.options != files) {
 			dropdown.options = files;
 			dropdown.RefreshShownValue ();
@@ -94,7 +105,9 @@ public class FilePicker : MonoBehaviour {
 	}
 
 	public string getStringFromFile(FileInfo file) {
+	//#Stop if an error is encountered
         if (!file.Exists) return "File not found.";
+	//#Read file
 		StreamReader sr = file.OpenText();
 		string data = sr.ReadToEnd ();
 		sr.Close ();
@@ -102,23 +115,28 @@ public class FilePicker : MonoBehaviour {
 	}
 
 	public void loadFromSelectedFile() {
+	//Stop if an error is encountered
 		if (file == null)
 			return;
-		string raw = getStringFromFile (file);
+	//
+		string raw = getStringFromFile (file);\
+	//By deafult, data is invalid. If a version tag is included, it becomes valid data.
 		bool valid = false;
 		foreach (string line in raw.Split(new string[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)) {
 			if (line.Split (';') [0] == "Version")
 				valid = true;
 		}
-		if (!valid) {
-			resultText.text = "Error: File is not valid, missing Version tag.";
-            Debug.LogWarning("Error: File is not valid, missing Version tag. Raw data: " + raw);
-			return;
+		//If error is encountered, send warning message and stop.
+			if (!valid) {
+				resultText.text = "Error: File is not valid, missing Version tag.";
+        	    Debug.LogWarning("Error: File is not valid, missing Version tag. Raw data: " + raw);
+				return;
 		}
+	//
 		foreach (string line in raw.Split(new string[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)) {
 			if (line == null)
 				continue;
-			string[] broken = line.Split (';');
+			string[] broken = line.Split (';'); //Data split by the ";" character.
 			if (broken [0].Equals ("Version") && broken [1] != DS.data ["Version"]) {
 				resultText.text = "Error: Version mismatch between " + DS.data ["Version"] + " and " + broken [1];
 				return;
@@ -134,6 +152,7 @@ public class FilePicker : MonoBehaviour {
     public void clearFiles()
     {
         DirectoryInfo directory = new DirectoryInfo(Application.persistentDataPath);
+	//#Delete local .txt or .png files in directory
         foreach (FileInfo file in directory.GetFiles())
         {
             if (file.Extension == ".txt" || file.Extension == ".png")
@@ -143,6 +162,7 @@ public class FilePicker : MonoBehaviour {
                 resultText.text = "Deleted file " + file.Name;
             }
         }
+	//Delete downloaded files
         resultText.text = "Finished deleting unuploaded files. Clearing uploaded files.";
         DirectoryInfo uploaded = new DirectoryInfo(Application.persistentDataPath + Path.DirectorySeparatorChar + "uploaded");
         if (uploaded.Exists)
